9cf33170d67a1e05d4526e0c5c2f6c072585388d
adamzoiss <adamzoiss@gmail.com>
2025-01-27 10:33:37 -0500

    Create file structure

---
+++ audio_processing/audio_handler.py
@@ -0,0 +1,14 @@
+"""
+This file will handle:
+* recording
+* playing
+* bit-stream of audio
+
+This will be done using a usb mic & headphones.
+"""
+
+# Libraries (imports)
+
+
+if __name__ == "__main__":
+    print("This is the audio driver")
